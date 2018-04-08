7ca01ceb342967a8d15d5bce78e4fa9995456306
ggggle <jccj@qq.com>
2018-04-08 21:12:09 +0800

    更新编译脚本

--- make.sh
+++ make.sh
@@ -2,9 +2,9 @@
 
 echo -n "\033]0;Auto Make\007"
 
-FNAME=luzhibo
-PNAME=github.com/Baozisoftware/$FNAME
-GPATH=https://github.com/Baozisoftware/luzhibo.git
+FNAME=liveGet
+PNAME=github.com/ggggle/$FNAME
+GPATH=https://github.com/ggggle/liveGet.git
 CPATH=`pwd`
 BPATH=`dirname $0`
 UPX=$BPATH/upx
@@ -34,26 +34,21 @@ DONE()
 	exit 0
 }
 
-INIT()
-{
-	#init
-	echo Initing...
-	go get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
-	go get github.com/PuerkitoBio/goquery
-	go get github.com/pkg/browser
-	go get github.com/Baozisoftware/qrcode-terminal-go
-	go get github.com/mattn/go-isatty
-	go get github.com/lxn/walk
-	go get github.com/dkua/go-ico
-	go get gopkg.in/Knetic/govaluate.v3
-	go get github.com/lxn/win
-	go get github.com/inconshreveable/go-update
-	go get github.com/Baozisoftware/GoldenDaemon
-	go get github.com/Baozisoftware/golibraries
-}
-
+#init
+echo Initing...
+go get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+go get github.com/PuerkitoBio/goquery
+go get github.com/pkg/browser
+go get github.com/Baozisoftware/qrcode-terminal-go
+go get github.com/mattn/go-isatty
+go get github.com/lxn/walk
+go get github.com/dkua/go-ico
+go get gopkg.in/Knetic/govaluate.v3
+go get github.com/lxn/win
+go get github.com/inconshreveable/go-update
+go get github.com/Baozisoftware/GoldenDaemon
+go get github.com/Baozisoftware/golibraries
 if [ "$1" = "init" ]; then
-	INIT
 	DONE
 fi
 if [ "$GOPATH" = "" ]; then 
@@ -75,7 +70,17 @@ mkdir releases
 #386:7
 GOARCH=386
 
+GOOS=darwin
+MAKE
+GOOS=freebsd
+MAKE
 GOOS=linux
+MAKE
+GOOS=netbsd
+MAKE
+GOOS=openbsd
+MAKE
+GOOS=plan9
 MAKE
 GOOS=windows
 MAKE
@@ -83,9 +88,83 @@ MAKE
 #amd64:9
 GOARCH=amd64
 
+GOOS=darwin
+MAKE
+GOOS=dragonfly
+MAKE
+GOOS=freebsd
+MAKE
 GOOS=linux
 MAKE
+GOOS=netbsd
+MAKE
+GOOS=openbsd
+MAKE
+GOOS=plan9
+MAKE
+GOOS=solaris
+MAKE
 GOOS=windows
+MAKE
+
+#arm:6
+GOARCH=arm
+
+GOOS=android
+MAKE
+GOOS=darwin
+MAKE
+GOOS=freebsd
+MAKE
+GOOS=linux
+MAKE
+GOOS=netbsd
+MAKE
+GOOS=openbsd
+MAKE
+
+#arm64:2
+GOARCH=arm64
+
+GOOS=darwin
+MAKE
+GOOS=linux
+MAKE
+
+#mips:1
+GOARCH=mips
+
+GOOS=linux
+MAKE
+
+#mipsle:1
+GOARCH=mipsle
+
+GOOS=linux
+MAKE
+
+#mips64:1
+GOARCH=mips64
+
+GOOS=linux
+MAKE
+
+#mips64le:1
+GOARCH=mips64le
+
+GOOS=linux
+MAKE
+
+#ppc64:1
+GOARCH=ppc64
+
+GOOS=linux
+MAKE
+
+#ppc64le:1
+GOARCH=ppc64le
+
+GOOS=linux
 MAKE
 
 DONE
